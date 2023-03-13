5598ae2238b15e3c8205a31dc53fea56fd598a4f
AndyYoung <yangliu@oppo.com>
2023-03-13 23:26:24 +0800

    GPUImageFilter basic ok

---
+++ app/src/main/java/com/example/gpuimagetest/GPUImageRenderer.java
@@ -0,0 +1,194 @@
+package com.example.gpuimagetest;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GPUImageRenderer implements GLSurfaceView.Renderer {
+    public static final int NO_IMAGE = -1;
+    static final float CUBE[] = {
+            -1.0f, -1.0f,
+            1.0f, -1.0f,
+            -1.0f, 1.0f,
+            1.0f, 1.0f,
+    };
+    public static final float TEXTURE_NO_ROTATION[] = {
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+    };
+
+    private GPUImageFilter mFilter;
+
+    public final Object mSurfaceChangedWaiter = new Object();
+
+    private int mGLTextureId = NO_IMAGE;
+    private SurfaceTexture mSurfaceTexture = null;
+    private final FloatBuffer mGLCubeBuffer;
+    private final FloatBuffer mGLTextureBuffer;
+    private IntBuffer mGLRgbBuffer;
+
+    private int mOutputWidth;
+    private int mOutputHeight;
+    private int mImageWidth;
+    private int mImageHeight;
+    private int mAddedPadding;
+
+    private final Queue<Runnable> mRunOnDraw;
+    private final Queue<Runnable> mRunOnDrawEnd;
+    private boolean mFlipHorizontal;
+    private boolean mFlipVertical;
+
+    public GPUImageRenderer(final GPUImageFilter filter) {
+        mFilter = filter;
+        mRunOnDraw = new LinkedList<Runnable>();
+        mRunOnDrawEnd = new LinkedList<Runnable>();
+
+        mGLCubeBuffer = ByteBuffer.allocateDirect(CUBE.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mGLCubeBuffer.put(CUBE).position(0);
+
+        mGLTextureBuffer = ByteBuffer.allocateDirect(TEXTURE_NO_ROTATION.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mGLTextureBuffer.put(TEXTURE_NO_ROTATION).position(0);
+    }
+
+    @Override
+    public void onSurfaceCreated(final GL10 unused, final EGLConfig config) {
+        GLES20.glClearColor(0, 0, 0, 1);
+        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+        mFilter.init();
+    }
+
+    @Override
+    public void onSurfaceChanged(final GL10 gl, final int width, final int height) {
+        mOutputWidth = width;
+        mOutputHeight = height;
+        GLES20.glViewport(0, 0, width, height);
+        GLES20.glUseProgram(mFilter.getProgram());
+        mFilter.onOutputSizeChanged(width, height);
+        synchronized (mSurfaceChangedWaiter) {
+            mSurfaceChangedWaiter.notifyAll();
+        }
+    }
+
+    @Override
+    public void onDrawFrame(final GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        runAll(mRunOnDraw);
+        mFilter.onDraw(mGLTextureId, mGLCubeBuffer, mGLTextureBuffer);
+        runAll(mRunOnDrawEnd);
+        if (mSurfaceTexture != null) {
+            mSurfaceTexture.updateTexImage();
+        }
+    }
+
+    private void runAll(Queue<Runnable> queue) {
+        synchronized (queue) {
+            while (!queue.isEmpty()) {
+                queue.poll().run();
+            }
+        }
+    }
+
+    public void setFilter(final GPUImageFilter filter) {
+        runOnDraw(new Runnable() {
+
+            @Override
+            public void run() {
+                final GPUImageFilter oldFilter = mFilter;
+                mFilter = filter;
+                if (oldFilter != null) {
+                    oldFilter.destroy();
+                }
+                mFilter.init();
+                GLES20.glUseProgram(mFilter.getProgram());
+                mFilter.onOutputSizeChanged(mOutputWidth, mOutputHeight);
+            }
+        });
+    }
+
+    public void deleteImage() {
+        runOnDraw(new Runnable() {
+
+            @Override
+            public void run() {
+                GLES20.glDeleteTextures(1, new int[]{mGLTextureId}, 0);
+                mGLTextureId = NO_IMAGE;
+            }
+        });
+    }
+
+    public void setImageBitmap(final Bitmap bitmap) {
+        setImageBitmap(bitmap, true);
+    }
+
+    public void setImageBitmap(final Bitmap bitmap, final boolean recycle) {
+        if (bitmap == null) {
+            return;
+        }
+
+        runOnDraw(new Runnable() {
+
+            @Override
+            public void run() {
+                Bitmap resizedBitmap = null;
+                if (bitmap.getWidth() % 2 == 1) {
+                    resizedBitmap = Bitmap.createBitmap(bitmap.getWidth() + 1, bitmap.getHeight(),Bitmap.Config.ARGB_8888);
+                    Canvas can = new Canvas(resizedBitmap);
+                    can.drawARGB(0x00, 0x00, 0x00, 0x00);
+                    can.drawBitmap(bitmap, 0, 0, null);
+                    mAddedPadding = 1;
+                } else {
+                    mAddedPadding = 0;
+                }
+
+                mGLTextureId = OpenGlUtils.loadTexture(resizedBitmap != null ? resizedBitmap : bitmap, mGLTextureId, recycle);
+                if (resizedBitmap != null) {
+                    resizedBitmap.recycle();
+                }
+                mImageWidth = bitmap.getWidth();
+                mImageHeight = bitmap.getHeight();
+            }
+        });
+    }
+
+    protected int getFrameWidth() {
+        return mOutputWidth;
+    }
+
+    protected int getFrameHeight() {
+        return mOutputHeight;
+    }
+
+
+
+    protected void runOnDraw(final Runnable runnable) {
+        synchronized (mRunOnDraw) {
+            mRunOnDraw.add(runnable);
+        }
+    }
+
+    protected void runOnDrawEnd(final Runnable runnable) {
+        synchronized (mRunOnDrawEnd) {
+            mRunOnDrawEnd.add(runnable);
+        }
+    }
+}
